80b77a49996fcab3fe82d1ea47b1bff17ea40c6a
Alexander Barriga <45.al3x@gmail.com>
2018-03-13 02:42:17 -0600

    Added templates

--- modules/appModule.js
+++ modules/appModule.js
@@ -1,38 +1,52 @@
 var app = angular.module("appModule", ["ngRoute"]);
-app.config(function($routeProvider){
-    $routeProvider
-    .when("/",{
-        templateUrl: "templates/home.html"
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      templateUrl: "templates/home.html"
     })
-    .when("/academic",{
+    .when("/academic", {
       controller: "academicController",
       templateUrl: "templates/academic.html"
     })
-    .when("/proyects/:proyect",{
-        controller: "detalleController",
-        templateUrl: "templates/detalle.html"
+    .when("/project_Montrer", {
+      controller: "projectController",
+      templateUrl: "templates/montrer.html"
+    })
+    .when("/project_Ficm", {
+      controller: "projectController",
+      templateUrl: "templates/ficm.html"
     })
     .otherwise("/");
 });
 
-app.controller("academicController", function($location, $scope) {
+app.controller("academicController", function ($location, $scope) {
   $scope.scrollTo = function (hash) {
     $location.hash(hash);
   };
-  $scope.isActive = function(route) {
+  $scope.isActive = function (route) {
     console.log(route);
     return route === $location.path();
   };
 });
 
-app.controller("navbarController", function($location, $scope) {
-  $scope.isActive = function(route) {
+app.controller("projectController", function ($location, $scope) {
+  $scope.scrollTo = function (hash) {
+    $location.hash(hash);
+  };
+  $scope.isActive = function (route) {
+    console.log(route);
     return route === $location.path();
   };
 });
 
-app.controller("detalleController", function($scope, $http, $routeParams){
+app.controller("navbarController", function ($location, $scope) {
+  $scope.isActive = function (route) {
+    return route === $location.path();
+  };
+});
 
-    $scope.parametroUrl = $routeParams.nameHola;
+app.controller("detalleController", function ($scope, $http, $routeParams) {
+
+  $scope.parametroUrl = $routeParams.nameHola;
 
 });
